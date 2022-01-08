71a92f1b35e673f9d460388d480d46cb0f261a6f
luusteve <luu.stevenluu@gmail.com>
2022-01-08 15:16:07 -0500

    Initial commit of the backend application

---
+++ src/test/java/sluu/sodapop/SodapopApplicationTests.java
@@ -0,0 +1,13 @@
+package sluu.sodapop;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SodapopApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
